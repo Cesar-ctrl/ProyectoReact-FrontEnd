c885851567e7c59b23edb9dba39df12b8d5361e0
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-09-30 09:57:12 +0200

    Comienzo de actualización Diseño
    Empezando por Perfil

--- src/components/pages/Miperfil.jsx
+++ src/components/pages/Miperfil.jsx
@@ -22,90 +22,62 @@ const Miperfil = () => {
         setLoggedIn(false)
         navigate("/welcome", { replace: true });
       }
+    //if(!(loggUserJSON)&&!(loggGuardJSON)){
+    //    alert('No has iniciado sesión')
+    //}
     return (
-        
         <section className="home busqueda">
-        <header className='titulo main'>
-            <h2>Perfil</h2>
-        </header>
-        <section className='buscador'>
-            <div className='barra'>
-                
-            </div>
-            <div className='filtros'>
+            <header className='titulo main'>
+                <h2>Perfil</h2>
+            </header>
 
-            </div>
-        </section>
-
-        <div className='cuidador flexea'>
-            
-            { usuario? 
-            <Link  to="/home/perfil/personal" className="flexea color" state={state}>
-                <div className='foto'>
+            <section className="perfil">
+                    
+                { usuario? 
+                <Link  to="/home/perfil/personal" className="flexea color centertext" state={state}>
                     <img src="../img/cuenta.svg" className='fotoestandar' alt="" />
-                </div>
-                <div className='nombreval flexea centertext'>
                     <h3>Información personal</h3>
-                </div>
-            </Link> :
-            guard?
-                <Link  to="/home/perfil/guardpersonal" className="flexea color" state={state}>
-                    <div className='foto'>
+                </Link> : null
+                }
+                {
+                guard?
+                    <Link  to="/home/perfil/guardpersonal" className="flexea color centertext" state={state}>
                         <img src="../img/cuenta.svg" className='fotoestandar' alt="" />
-                    </div>
-                    <div className='nombreval flexea centertext'>
                         <h3>Información personal</h3>
-                    </div>
-                </Link> 
-                :
-                alert('No has iniciado sesión')
-            } 
-        </div>
-        <div className='cuidador flexea'>
-            <Link to="/home/perfil/ajustes" className="flexea color" state={state}>
-                <div className='foto'>
-                    <img src="../img/settings.svg" className='fotoestandar' alt="" />
-                </div>
-                <div className='nombreval flexea centertext'>
-                    <h3>Ajustes</h3>
-                </div>
-            </Link>
-        </div>
-        <div className='cuidador flexea'>
-            <div className='foto'>
-                <img src="../img/info.svg" className='fotoestandar' alt="" />
-            </div>
-            <div className='nombreval flexea centertext'>
-                <h3>Terminos y condiciones de uso</h3>
-            </div>
-        </div>
-        <div className='cuidador flexea'>
-            <div className='foto'>
-                <img src="../img/info.svg" className='fotoestandar' alt="" />
-            </div>
-            <div className='nombreval flexea centertext'>
-                <h3>Acerca de</h3>
-            </div>
-        </div>
+                    </Link> 
+                    : null
+                } 
+                
+                <Link to="/home/perfil/ajustes" className="flexea color centertext" state={state}>                     
+                    <img src="../img/settings.svg" className='fotoestandar' alt="" /> 
+                    <h3>Ajustes</h3>                   
+                </Link>
 
-        <div className='cuidador flexea'>
-                <div className='foto'>
+                <a className='flexea color centertext'>                  
+                    <img src="../img/info.svg" className='fotoestandar' alt="" />
+                    <h3>Terminos y condiciones de uso</h3>              
+                </a>
+
+                <a className='flexea color centertext'>
+                    <img src="../img/info.svg" className='fotoestandar' alt="" />               
+                    <h3>Acerca de</h3>                  
+                </a>
+
+                <div className='flexea color centertext salir' onClick={handleLogout} >                       
                     <img src="../img/logout.svg" className='fotoestandar' alt="" />
-                </div>
-                <div className='nombreval flexea centertext'onClick={handleLogout} >
                     {
-                        usuario?
-                            <h3>Cerrar sesión</h3>
-                            : guard?
-                            <h3>Cerrar sesión</h3>
-                            :
-                            <h3>Volver al Registro</h3>
+                    usuario?
+                        <h3>Cerrar sesión</h3>
+                        : guard?
+                        <h3>Cerrar sesión</h3>
+                        :
+                        <h3>Volver al Registro</h3>
                     }
+                        
                 </div>
-        </div>
+            </section>
 
-
-      </section>
+        </section>
     )
 }
 export default Miperfil
