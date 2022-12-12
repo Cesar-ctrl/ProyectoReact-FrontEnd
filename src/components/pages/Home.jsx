d2ddda399bccacf18067572abcb14b5afd4dbc89
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-12-12 16:58:21 +0100

    localhost to api

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -51,23 +51,6 @@ function Home() {
           }  
     }, [])
     
-    //useEffect(() => { //utilizamos socket para que recibir notificaciones
-    //    // cambiar a http://localhost:3001 Si se quiere probar en local 
-    //    // Y en el servidor tambén pondré un comentario de que cambiar para que funcione en local 
-    //    if(loggGuardJSON || loggedUserJSON){
-    //        socket.current = io('https://babyguard.onrender.com');
-    //        if (loggGuardJSON) {
-    //            socket.current.emit("add-user", loggGuardJSON.id)
-    //        }
-    //        if(loggedUserJSON){
-    //            socket.current.emit("add-user", loggedUserJSON.id)
-    //        }
-    //    }
-    //    //Continuar con las notificaciones
-    //    
-    //    
-    //}, []);
-    //console.log(socket)
 
     const toggleback = () => {
         setContainer('container backunder')
